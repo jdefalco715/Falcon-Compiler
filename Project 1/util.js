6bbbab2dbf1b56550170796e1d5bcca097e75bf0
John <john.defalco1@marist.edu>
2019-09-17 13:49:46 -0400

    Added trim utility function

---
+++ Project 1/util.js
@@ -0,0 +1,9 @@
+// Utility functions
+
+
+// Trim function used from example js compiler
+function trim(str) {
+	// Removes leading and trailing whitespaces in selected string
+	// For this project, the string would be the source code meant to compile
+	return str.replace(/^\s | \s+$/g, "");
+}
