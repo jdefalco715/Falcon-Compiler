3e31b29580dbdc8edd7dd54f797c9cea31bccc8d
John <john.defalco1@marist.edu>
2019-09-17 14:40:53 -0400

    Started writing begining of lexer in lexer.js file

---
+++ Project 1/lexer.js
@@ -0,0 +1,12 @@
+// Lex file
+
+function lexer(){
+
+	// Array of tokens for parsing
+	var tokens = [];
+
+	// Variable that recieves source code as string
+	// Id is marked as 'Source Code', will be defined in HTML file
+	// HTML file has not been implemented yet
+	var source = document.getElementById("SourceCode").value;
+}
