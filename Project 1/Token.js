834b21e7a619312e56b16273c9f8f8cb1f4df73c
John <john.defalco1@marist.edu>
2019-09-17 14:54:45 -0400

    Edits on lexer.js file, implemented token constructor on token.js file

---
+++ Project 1/Token.js
@@ -0,0 +1,14 @@
+// Token definitions
+
+/* Constructor for token object
+ * Parameter type defines rank of token 
+ * Parameter kind defines name of token
+ * Parameter line defines the line that the token was found
+ * Parameter column defines position of first char of token
+ */
+var Token(type, kind, line, column) {
+	this.type = type;
+	this.kind = kind;
+	this.line = line;
+	this.column = column;
+}
--- Project 1/lexer.js
+++ Project 1/lexer.js
@@ -1,12 +1,17 @@
 // Lex file
 
-function lexer(){
+
+
+//Function takes input as parameter
+function lexer(input){
 
 	// Array of tokens for parsing
 	var tokens = [];
 
-	// Variable that recieves source code as string
-	// Id is marked as 'Source Code', will be defined in HTML file
-	// HTML file has not been implemented yet
-	var source = document.getElementById("SourceCode").value;
+	/* // Variable that recieves source code as string
+	   // Id is marked as 'Source Code', will be defined in HTML file
+	   // HTML file has not been implemented yet
+	   var source = document.getElementById("SourceCode").value;
+	*/
+	
 }
