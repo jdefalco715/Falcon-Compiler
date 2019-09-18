50401e569decd9a3b8aa8db01bb436c41a5c9386
John <john.defalco1@marist.edu>
2019-09-18 09:57:25 -0500

    Added token defs

--- Project 1/lexer.js
+++ Project 1/lexer.js
@@ -4,14 +4,54 @@
 var tokens = [];
 
 //Function takes input as parameter
-function lexer(input){
+function lexer(){
+	// Variable that recieves source code as string
+	// Id is marked as 'Source Code', will be defined in HTML file
+	// HTML file has not been implemented yet
+	var source = document.getElementById("sc").value;
 
-	
+	// Trim whitespace
+	source = trim(source);
 
-	/* // Variable that recieves source code as string
-	   // Id is marked as 'Source Code', will be defined in HTML file
-	   // HTML file has not been implemented yet
-	   var source = document.getElementById("SourceCode").value;
-	*/
-	
+	// Defining tokens from the class grammar
+	// EOP
+	var EOP = /'\$$'/;
+	// Left brace
+	var L_BRACE = /'{$'/;
+	// Right brace
+	var R_BRACE = /'}$'/;
+	// Left paren
+	var L_PAREN = /'\($'/;
+	// Right paren
+	var R_PAREN = /'\)$'/;
+	// Print
+	var PRINT = /'print$'/;
+	// While
+	var WHILE = /'while$'/;
+	// If
+	var IF = /'if$'/;
+	// Quotation mark
+	var QUOTE = /'"$'/; 
+	// Int
+	var INT = /'int$'/;
+	// String
+	var STRING = /'string$'/;
+	// Boolean
+	var BOOLEAN = /'boolean$'/;
+	// Char 
+	var CHAR = /'[a-z]$'/;
+	// Id
+	var ID = /'[a-z]$'/;
+	// Space
+	var SPACE = /'\s$'/;
+	// Digit
+	var DIGIT = /'[0-9]+$'/;
+	// Equals Boolean Op
+	var EQUALS = /'\=\=$'/;
+	// Not Equals Boolean Op
+	var NOT_EQUALS = /'\!\=$'/;
+	// Addition Operator
+	var ADD = /'\+$'/;
+	// Assign Operator
+	var ASSIGN = /'\='/;
 }
